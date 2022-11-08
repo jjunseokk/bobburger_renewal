e9c3e0000e0ec677b3dc20447641ac02b71b62e3
junseok park <p.junseok53@gmail.com>
2022-11-08 10:47:14 +0900

    제발 돼라.

--- src/page/Menu.js
+++ src/page/Menu.js
@@ -4,7 +4,7 @@ import MenuCard from '../component/MenuCard';
 
 const Menu = () => {
   
-  const [productList, setProductList] = useState();
+  const [productList, setProductList] = useState([]);
   const getProducts= async() => {
     let url = `https://my-json-server.typicode.com/jjunseokk/bobburger_renewal/products`;
     let respanse = await fetch(url,{maxContentLength : 100000});
@@ -24,7 +24,7 @@ const Menu = () => {
           <img className='topigImg' src="http://www.bongousse.net/img/menu/newimg/menu_topping_guide-20220901.png" alt=""/>
         </div>
         <Row>
-          {true && productList.map((menu)=>(
+          {productList.map((menu)=>(
             <Col lg={3}>
               <MenuCard item={menu}/>
             </Col>
